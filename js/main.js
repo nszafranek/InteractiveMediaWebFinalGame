067e171e395a419ebc61892a03045f8fa51458bc
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-04-13 19:02:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ What's broken
 function setup() {
   //if mobile
   if ((screen.width <= 768) /*&& (screen.orientation === 'portrait-primary')*/) {
-    cnv = createCanvas(450, 400);
+    cnv = createCanvas(425, 500);
     cnv.id('gameCanvas')
   }
   /*else if ((screen.height <= 768) && (screen.orientation === 'landscape-primary')) {
