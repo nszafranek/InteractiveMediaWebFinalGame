b678c19ffda25034aae7efe6b8904be717f23b66
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-04-14 11:12:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ function setup() {
     cnv.id('gameCanvas')
   }
   else if ((screen.height <= 800) && (screen.orientation === 'landscape-primary')) {
-    cnv = createCanvas(500, 230);
+    cnv = createCanvas(450, 200);
     cnv.id('gameCanvas');
   }
   //otherwise
