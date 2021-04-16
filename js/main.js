92959d8f0a59765bed874532ae292dd70ea94c81
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-04-16 12:02:53 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -203,9 +203,9 @@ function draw() {
       text("Infinite Swimmer", (width / 2), ((height / 2) - 40));
       textSize(14);
       strokeWeight(1);
-      text("Use the Arrow Keys or swipe in one direction to move the hero!", (width / 2), ((height / 2) + 10));
-      text("Press Esc to end", (width / 2), ((height / 2) + 30));
-      text("Avoid the Sharks and collect Gems and Coins!", (width / 2), ((height / 2) + 60));
+      text("Use the Arrow Keys or swipe in one direction to move the hero!", (width / 2), ((height / 2) + 15));
+      text("Press Esc to end", (width / 2), ((height / 2) + 25));
+      text("Avoid the Sharks and collect Gems and Coins!", (width / 2), ((height / 2) + 35));
       textSize(30);
       strokeWeight(8);
       text("Press Enter or tap the screen to start!", (width / 2), ((height / 2) + 90));
