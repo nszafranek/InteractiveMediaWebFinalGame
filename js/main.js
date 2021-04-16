24023d4fda31343868fe8cc9631aec38b3a492e3
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-04-16 17:33:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -225,7 +225,7 @@ function draw() {
       textAlign(CENTER);
       textFont(gameFont);
       strokeWeight(1);
-      textSize(25);
+      textSize(18);
       strokeWeight(7);
       text("Infinite Swimmer", (width / 2), ((height / 2) - 30));
       textSize(10);
