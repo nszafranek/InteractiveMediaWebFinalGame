1703dae2ae2572f8b1b5558285b3a4cd7944add4
nszafranek <43145639+nszafranek@users.noreply.github.com>
2021-04-15 22:50:35 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -219,7 +219,7 @@ function draw() {
       strokeWeight(5);
       text("Use the Arrow Keys or swipe in one direction to move the hero", (width / 2), (height / 2));
       text("Press Esc to end", (width / 2), ((height / 2) - 20));
-      text("Avoid the Sharks and collect Gems and Coins!", (width / 2), ((height / 2) + 40));
+      text("Avoid the Sharks and collect Gems and Coins!", (width / 2), ((height / 2) - 40));
       textSize(30);
       strokeWeight(3);
       text("Press Enter or tap the screen to start!", width / 2, (height / 2) + 125);
